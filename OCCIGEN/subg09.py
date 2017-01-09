d54b89f6a8c86d197441a919e4fdb75c9b499898
Emmanuel <emmanuel.nicolas@polytechnique.edu>
2017-01-09 11:40:41 +0100

    Missing rwf extension

--- OCCIGEN/subg09.py
+++ OCCIGEN/subg09.py
@@ -279,7 +279,7 @@ def create_run_file(input_file, output, runvalues):
                 '    mkdir -p $SCRATCHDIR/gaussian/rwf\n'
                 # Copy rwf as JobName_123456.rwf
                 '    [ -f "$f" ] && cp $f $SCRATCHDIR/gaussian/rwf/' +
-                shlexnames['basename'] + '_$SLURM_JOBID\n',
+                shlexnames['basename'] + '_$SLURM_JOBID.rwf\n',
                 'done\n',
                 '\n',
                 '# Empty Scratch directory\n',
