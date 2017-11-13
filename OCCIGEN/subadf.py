e646b8e1422eee8bb696865cb202532552c9a2e1
Emmanuel <LaTruelle@users.noreply.github.com>
2017-11-13 15:23:58 +0100

    Update from Gaussian Script

--- OCCIGEN/subadf.py
+++ OCCIGEN/subadf.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Submit script for Gaussian09 for Computing Clusters
-Original script for LISA by Jos Mulder
-email j.r.mulder -at- vu.nl
-Adapted for Cines OCCIGEN cluster
-Last Update 2016-02-04 by Emmanuel Nicolas
+Submit script for Gaussian09 for Computing Clusters.
+
+Script adapted for Cines OCCIGEN cluster
+Last Update 2017-10-30 by Emmanuel Nicolas
 email emmanuel.nicolas -at- cea.fr
 Requires Python3 to be installed.
 """
@@ -14,61 +13,105 @@ Requires Python3 to be installed.
 import argparse
 import sys
 import os
+import logging
 import shlex
+import re
 
 
 def main():
     """
-        Main function.
-        Checks existence of input, and non-existence of batch file (e.g.
-        computation has not already been submitted).
-        Run computation
+    Set up the computation and submit it to the job scheduler.
+
+    Precedence of parameters for submission:
+        - Command line parameters
+        - Gaussian script
+        - Default parameters
+
+    Structure of program:
+    - Define runvalues:
+        - Fill Defaults
+        - Parse file and replace appropriately
+        - Get command-line parameters and replace appropriately
+    - Compute missing values
+        - Memory
+        - Number of nodes if appropriate
+        - Cluster section (HSW24/BDW28)
+        - Convert filenames to printable values
+    - Check parameters
+        - Existence of files (or non-existence...)
+        - Compatibility nproc/nodes/memory
+    - Build script file
+    - Submit script
     """
+    # Setup logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    # Setup runvalues with default settings
+    runvalues = default_run_values()
+
     # Get parameters from command line
-    runvalues = get_options()
+    cmdline_args = get_options()
+
     # Retrieve input file name, create output file name
-    input_file_name = runvalues['inputfile']
-    output = input_file_name + ".sh"
+    input_file_name = cmdline_args['inputfile']
+    script_file_name = input_file_name + ".sh"
+
     # Check existence of input file
     if not os.path.exists(input_file_name):
         print("=========== !!! WARNING !!! ===========")
         print("The input file was not found.")
         sys.exit()
     # Check that file input.sh does not exist, not to start twice the same job
-    if os.path.exists(output):
+    if os.path.exists(script_file_name):
         print("=========== !!! WARNING !!! ===========")
         print(" The corresponding .sh file already exists ")
         print("Make sure it is not a mistake, erase it and rerun the script")
         print("Alternatively, you can submit the job directly with:")
-        print("sbatch {0}".format(shlex.quote(output)))
+        print("sbatch {0}".format(shlex.quote(script_file_name)))
         sys.exit()
     # Avoid end of line problems due to conversion between Windows and Unix
     # file endings
     os.system('dos2unix {0}'.format(shlex.quote(input_file_name)))
-    # Backfill parameters not set in the runvalues
-    runvalues = get_defaultvalues(runvalues)
+
+    # Get computation parameters from input file
+    runvalues = get_values_from_input_file(input_file_name, runvalues)
+
+    # Merge command-line parameters into runvalues
+    runvalues = fill_from_commandline(runvalues, cmdline_args)
+
+    # Fill with missing values and consolidate the whole thing
+    runvalues = fill_missing_values(runvalues)
+
     # Create run file for gaussian
-    create_run_file(input_file_name, output, runvalues)
+    create_run_file(script_file_name, runvalues)
     # Submit the script
-    os.system('sbatch {0}'.format(shlex.quote(output)))
+    os.system('sbatch {0}'.format(shlex.quote(script_file_name)))
     print("job {0} submitted with a walltime of {1} hours"
-          .format(input_file_name, runvalues["walltime"]))
+          .format(input_file_name, runvalues['walltime']))
 
 
 def get_options():
-    """
-        Check command line options and accordingly set computation parameters
-    """
+    """Check command line options and accordingly set computation parameters."""
     parser = argparse.ArgumentParser(description=help_description(),
                                      epilog=help_epilog())
     parser.formatter_class = argparse.RawDescriptionHelpFormatter
-    parser.add_argument('-n', '--nproc', default=24, type=int,
-                        help="Number of cores used for the computation")
+    parser.add_argument('-p', '--proc', type=int,
+                        help="Number of processors used for the computation")
+    parser.add_argument('-n', '--nodes', type=int,
+                        help="Number of nodes used for the computation")
     parser.add_argument('-t', '--walltime', default="24:00:00", type=str,
                         help="Maximum time allowed for the computation")
     parser.add_argument('-m', '--memory', type=int,
-                        help="Amount of memory allowed for the computation")
-    parser.add_argument('inputfile', type=str, nargs='+',
+                        help="Amount of memory allowed for the computation, in MB")
+    parser.add_argument('inputfile', type=str, nargs=1,
                         help='The input file to submit')
 
     try:
@@ -99,39 +142,99 @@ def get_defaultvalues(runvalues):
     return runvalues
 
 
-def create_shlexnames(input_file):
-    """
-        Return dictionary containing shell escaped names for all possible files
-    """
+def fill_missing_values(runvalues):
+    """Compute and fill all missing values."""
+    # Setup cluster_section according to number of cores
+    if runvalues['cores'] <= 24:
+        runvalues['cluster_section'] = "HSW24"
+    elif runvalues['cores'] <= 28:
+        runvalues['cluster_section'] = "BDW28"
+    elif runvalues['cores'] > 28 and runvalues['nodes'] == 1:
+        raise ValueError("Number of cores cannot exceed 28 for one node.")
+    elif runvalues['nodes'] > 1:
+        raise ValueError("Multiple nodes not supported at the moment.")
+
+    # TODO: manage the multiple nodes case
+
+    # TODO; Better memory checks
+    memory, gaussian_memory = compute_memory(runvalues)
+    runvalues['memory'] = memory
+    runvalues['gaussian_memory'] = gaussian_memory
+
+    return runvalues
+
+
+def create_shlexnames(runvalues):
+    """Return dictionary containing shell escaped names for all possible files."""
     shlexnames = dict()
-    input_basename = os.path.splitext(input_file)[0]
-    shlexnames['inputname'] = shlex.quote(input_file)
+    input_basename = os.path.splitext(runvalues['inputfile'])[0]
+    shlexnames['inputfile'] = shlex.quote(runvalues['inputfile'])
     shlexnames['basename'] = shlex.quote(input_basename)
+    if runvalues['chk'] is not None:
+        shlexnames['chk'] = [shlex.quote(chk) for chk in runvalues['chk']]
+    if runvalues['oldchk'] is not None:
+        shlexnames['oldchk'] = [shlex.quote(oldchk) for oldchk in runvalues['oldchk']]
+    if runvalues['rwf'] is not None:
+        shlexnames['rwf'] = [shlex.quote(rwf) for rwf in runvalues['rwf']]
     return shlexnames
 
 
-def create_run_file(input_file, output, runvalues):
+def compute_memory(runvalues):
     """
-        Create .sh file that contains the script to actually run on the server.
-        Structure:
-        -- SBATCH instructions for the queue manager
-        -- setup of ADF on the nodes
-        -- creation of scratch, copy necessary files
-        -- Run ADF
-        -- Copy and rename appropriate files back to $HOME
-        -- Cleanup scratch
+    Return ideal memory value for OCCIGEN.
 
-        Instructions adapted from www.cines.fr
+    4GB per core, or memory from input + 4000 MB for overhead.
+    Computed to use as close as possible the memory available.
     """
+    if runvalues['memory'] is not None:
+        # Memory already defined in input file
+        gaussian_memory = runvalues['memory']
+        # SLURM memory requirement is gaussian_memory + overhead, as long as
+        # it fits within the general node requirements
+        if gaussian_memory + 4000 < runvalues['cores'] * 4800:
+            memory = gaussian_memory + 4000
+        else:
+            memory = runvalues['cores'] * 4800
+    else:
+        gaussian_memory = runvalues['cores'] * 4000
+        memory = runvalues['cores'] * 4800
+
+    return (memory, gaussian_memory)
+
+
+def create_run_file(output, runvalues):
+    """
+    Create .sh file that contains the script to actually run on the server.
+
+    Structure:
+        - SBATCH instructions for the queue manager
+        - setup of Gaussian09 on the nodes
+        - creation of scratch, copy necessary files
+        - Run Gaussian09
+        - Copy appropriate files back to $HOME
+        - Cleanup scratch
+
+    Instructions adapted from www.cines.fr
+    """
+    # Setup logging
+    logger = logging.getLogger()
 
     # Setup names to use in file
-    shlexnames = create_shlexnames(input_file)
+    shlexnames = create_shlexnames(runvalues)
+    logger.debug("Runvalues:  %s", runvalues)
+    logger.debug("Shlexnames: %s", shlexnames)
 
+    # TODO: multi-nodes
+    # On SLURM, memory is defined per node
+    # #SBATCH --nodes=2
+    # #SBATCH --ntasks=48
+    # #SBATCH --ntasks-per-node=24
+    # #SBATCH --threads-per-core=1
     out = ['#!/bin/bash\n',
-           '#SBATCH -J ' + shlexnames['inputname'] + '\n',
+           '#SBATCH -J ' + shlexnames['inputfile'] + '\n',
+           '#SBATCH --constraint=' + runvalues['cluster_section'] + '\n'
            '#SBATCH --mail-type=ALL\n',
            '#SBATCH --mail-user=user@server.org\n',
-           '#SBATCH --constraint=HSW24\n',
            '#SBATCH --nodes=1\n',
            '#SBATCH --ntasks=' + str(runvalues['cores']) + '\n',
            '#SBATCH --mem=' + str(runvalues['memory']) + '\n',
@@ -142,6 +245,11 @@ def create_run_file(input_file, output, runvalues):
                 'module purge\n',
                 'module load adf/2016.102-openmpi',
                 '\n'])
+    if runvalues['nbo']:
+        out.extend(['# Setup NBO6\n',
+                    'export NBOBIN=$SHAREDHOMEDIR/nbo6/bin\n',
+                    'export PATH=$PATH:$NBOBIN\n',
+                    '\n'])
     out.extend(['# Setup Scratch\n',
                 'export SCM_TMPDIR=$SCRATCHDIR/adf/$SLURM_JOBID\n',
                 'mkdir -p $SCM_TMPDIR\n',
@@ -160,7 +268,7 @@ def create_run_file(input_file, output, runvalues):
     walltime = [int(x) for x in runvalues['walltime'].split(':')]
     runtime = max(3600 * walltime[0] + 60 * walltime[1] + walltime[2] - 60, 60)
     out.extend(['# Start ADF\n',
-                'timeout ' + str(runtime) + ' sh < ' + shlex.quote(input_file),
+                'timeout ' + str(runtime) + ' sh < ' + shlex.quote(shlexnames['inputname']),
                 ' > ' + shlexnames['basename'] + '.out\n',
                 '\n'])
     out.extend(['# Move files back to original directory\n',
@@ -197,9 +305,7 @@ def create_run_file(input_file, output, runvalues):
 
 
 def help_description():
-    """
-        Returns description of program for help message
-    """
+    """Return description of program for help message."""
     return """
 Setup and submit a job to the SLURM queueing system on the OCCIGEN cluster.
 The jobscript name should end with .gjf or .com.
@@ -207,13 +313,11 @@ The jobscript name should end with .gjf or .com.
 
 
 def help_epilog():
-    """
-        Returns additionnal help message
-    """
+    """Return additionnal help message."""
     return """
 Defaults values:
-  Default memory:          64 GB
-  Default cores:           24
+  Default memory:          1 GB
+  Default cores:           1
   Default walltime:        24:00:00
 
 Values for number of cores used and memory to use are read in the input file,
