693d8babb38ca74c30f78e3558ffe4e04e5ce62e
Emmanuel <emmanuel.nicolas_github@polytechnique.org>
2019-08-28 09:29:18 +0200

    Ran black on suborca

--- OCCIGEN/suborca.py
+++ OCCIGEN/suborca.py
@@ -48,7 +48,7 @@ def main():
 
     stream_handler = logging.StreamHandler()
 
-    formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+    formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(message)s")
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
@@ -60,7 +60,7 @@ def main():
     cmdline_args = get_options()
 
     # Retrieve input file name, create output file name
-    input_file_name = cmdline_args['inputfile']
+    input_file_name = cmdline_args["inputfile"]
     script_file_name = input_file_name + ".sh"
 
     # Check existence of input file
@@ -78,7 +78,7 @@ def main():
         sys.exit()
     # Avoid end of line problems due to conversion between Windows and Unix
     # file endings
-    os.system('dos2unix {0}'.format(shlex.quote(input_file_name)))
+    os.system("dos2unix {0}".format(shlex.quote(input_file_name)))
 
     # Get computation parameters from input file
     runvalues = get_values_from_input_file(input_file_name, runvalues)
@@ -99,26 +99,40 @@ def main():
     # Create run file for gaussian
     create_run_file(script_file_name, runvalues)
     # Submit the script
-    os.system('sbatch {0}'.format(shlex.quote(script_file_name)))
-    print("job {0} submitted with a walltime of {1} hours"
-          .format(input_file_name, runvalues['walltime']))
+    os.system("sbatch {0}".format(shlex.quote(script_file_name)))
+    print(
+        "job {0} submitted with a walltime of {1} hours".format(
+            input_file_name, runvalues["walltime"]
+        )
+    )
 
 
 def get_options():
     """Check command line options and accordingly set computation parameters."""
-    parser = argparse.ArgumentParser(description=help_description(),
-                                     epilog=help_epilog())
+    parser = argparse.ArgumentParser(
+        description=help_description(), epilog=help_epilog()
+    )
     parser.formatter_class = argparse.RawDescriptionHelpFormatter
-    parser.add_argument('-p', '--proc', type=int,
-                        help="Number of processors used for the computation")
-    parser.add_argument('-n', '--nodes', type=int,
-                        help="Number of nodes used for the computation")
-    parser.add_argument('-t', '--walltime', default="24:00:00", type=str,
-                        help="Maximum time allowed for the computation")
-    parser.add_argument('-m', '--memory', type=int,
-                        help="Amount of memory allowed for the computation, in MB")
-    parser.add_argument('inputfile', type=str, nargs=1,
-                        help='The input file to submit')
+    parser.add_argument(
+        "-p", "--proc", type=int, help="Number of processors used for the computation"
+    )
+    parser.add_argument(
+        "-n", "--nodes", type=int, help="Number of nodes used for the computation"
+    )
+    parser.add_argument(
+        "-t",
+        "--walltime",
+        default="24:00:00",
+        type=str,
+        help="Maximum time allowed for the computation",
+    )
+    parser.add_argument(
+        "-m",
+        "--memory",
+        type=int,
+        help="Amount of memory allowed for the computation, in MB",
+    )
+    parser.add_argument("inputfile", type=str, nargs=1, help="The input file to submit")
 
     try:
         args = parser.parse_args()
@@ -127,66 +141,76 @@ def get_options():
         sys.exit(2)
 
     # Get values from parser
-    cmdline_args = dict.fromkeys(['inputfile', 'walltime', 'memory', 'cores', 'nodes'])
-    cmdline_args['inputfile'] = os.path.basename(args.inputfile[0])
-    cmdline_args['walltime'] = args.walltime
+    cmdline_args = dict.fromkeys(["inputfile", "walltime", "memory", "cores", "nodes"])
+    cmdline_args["inputfile"] = os.path.basename(args.inputfile[0])
+    cmdline_args["walltime"] = args.walltime
     if args.proc:
-        cmdline_args['cores'] = args.proc
+        cmdline_args["cores"] = args.proc
     if args.nodes:
-        cmdline_args['nodes'] = args.nodes
+        cmdline_args["nodes"] = args.nodes
     if args.memory:
-        cmdline_args['memory'] = args.memory
+        cmdline_args["memory"] = args.memory
 
     return cmdline_args
 
 
 def fill_from_commandline(runvalues, cmdline_args):
     """Merge command line arguments into runvalues."""
-    runvalues['inputfile'] = cmdline_args['inputfile']
-    if cmdline_args['nodes']:
-        runvalues['nodes'] = cmdline_args['nodes']
-    if cmdline_args['cores']:
-        runvalues['cores'] = cmdline_args['cores']
-    if cmdline_args['walltime']:
-        runvalues['walltime'] = cmdline_args['walltime']
-    if cmdline_args['memory']:
-        runvalues['memory'] = cmdline_args['memory']
+    runvalues["inputfile"] = cmdline_args["inputfile"]
+    if cmdline_args["nodes"]:
+        runvalues["nodes"] = cmdline_args["nodes"]
+    if cmdline_args["cores"]:
+        runvalues["cores"] = cmdline_args["cores"]
+    if cmdline_args["walltime"]:
+        runvalues["walltime"] = cmdline_args["walltime"]
+    if cmdline_args["memory"]:
+        runvalues["memory"] = cmdline_args["memory"]
     return runvalues
 
 
 def default_run_values():
     """Fill default runvalues."""
     # Setup runvalues
-    runvalues = dict.fromkeys(['inputfile', 'outputfile', 'nodes', 'cores', 'walltime', 'memory',
-                               'nproc_in_input', 'cluster_section'])
-    runvalues['inputfile'] = ''
-    runvalues['outputfile'] = ''
-    runvalues['nodes'] = 1
-    runvalues['cores'] = 24
-    runvalues['walltime'] = '24:00:00'
-    runvalues['memory'] = 58000  # In MB
-    runvalues['nproc_in_input'] = False
-    runvalues['nbo'] = False
-    runvalues['nbo_basefilename'] = ''
-    runvalues['cluster_section'] = 'HSW24'
+    runvalues = dict.fromkeys(
+        [
+            "inputfile",
+            "outputfile",
+            "nodes",
+            "cores",
+            "walltime",
+            "memory",
+            "nproc_in_input",
+            "cluster_section",
+        ]
+    )
+    runvalues["inputfile"] = ""
+    runvalues["outputfile"] = ""
+    runvalues["nodes"] = 1
+    runvalues["cores"] = 24
+    runvalues["walltime"] = "24:00:00"
+    runvalues["memory"] = 58000  # In MB
+    runvalues["nproc_in_input"] = False
+    runvalues["nbo"] = False
+    runvalues["nbo_basefilename"] = ""
+    runvalues["cluster_section"] = "HSW24"
     return runvalues
 
 
 def get_values_from_input_file(input_file, runvalues):
     """Get core/memory values from input file, reading the Mem and NProcShared parameters."""
-    with open(input_file, 'r') as file:
+    with open(input_file, "r") as file:
         # Go through lines and test if they are containing nproc, mem, etc. related
         # directives.
         for line in file.readlines():
             if "nprocs" in line.lower():
-                runvalues['nproc_in_input'] = True
+                runvalues["nproc_in_input"] = True
                 proc_line = line.split()
-                runvalues['cores'] = int(proc_line[1])
+                runvalues["cores"] = int(proc_line[1])
             if "nbo6" in line.lower() or "npa6" in line.lower():
-                runvalues['nbo'] = True
+                runvalues["nbo"] = True
             if "FILE=" in line:
                 # FILE=FILENAME
-                runvalues['nbo_basefilename'] = line.split('=')[1].rstrip(' \n')
+                runvalues["nbo_basefilename"] = line.split("=")[1].rstrip(" \n")
 
     return runvalues
 
@@ -194,21 +218,21 @@ def get_values_from_input_file(input_file, runvalues):
 def fill_missing_values(runvalues):
     """Compute and fill all missing values."""
     # Setup cluster_section according to number of cores
-    if not runvalues['nproc_in_input']:
-        runvalues['cluster_section'] = "HSW24|BDW28"
-    elif runvalues['cores'] <= 24:
-        runvalues['cluster_section'] = "HSW24"
-    elif runvalues['cores'] <= 28:
-        runvalues['cluster_section'] = "BDW28"
-    elif runvalues['cores'] > 28 and runvalues['nodes'] == 1:
+    if not runvalues["nproc_in_input"]:
+        runvalues["cluster_section"] = "HSW24|BDW28"
+    elif runvalues["cores"] <= 24:
+        runvalues["cluster_section"] = "HSW24"
+    elif runvalues["cores"] <= 28:
+        runvalues["cluster_section"] = "BDW28"
+    elif runvalues["cores"] > 28 and runvalues["nodes"] == 1:
         raise ValueError("Number of cores cannot exceed 28 for one node.")
-    elif runvalues['nodes'] > 1:
+    elif runvalues["nodes"] > 1:
         raise ValueError("Multiple nodes not supported at the moment.")
 
     # TODO: manage the multiple nodes case
 
     # TODO; Better memory checks
-    runvalues['memory'] = compute_memory(runvalues)
+    runvalues["memory"] = compute_memory(runvalues)
 
     return runvalues
 
@@ -216,9 +240,9 @@ def fill_missing_values(runvalues):
 def create_shlexnames(runvalues):
     """Return dictionary containing shell escaped names for all possible files."""
     shlexnames = dict()
-    input_basename = os.path.splitext(runvalues['inputfile'])[0]
-    shlexnames['inputfile'] = shlex.quote(runvalues['inputfile'])
-    shlexnames['basename'] = shlex.quote(input_basename)
+    input_basename = os.path.splitext(runvalues["inputfile"])[0]
+    shlexnames["inputfile"] = shlex.quote(runvalues["inputfile"])
+    shlexnames["basename"] = shlex.quote(input_basename)
     return shlexnames
 
 
@@ -231,10 +255,10 @@ def compute_memory(runvalues):
     """
     memory = 0
 
-    if runvalues['memory'] is None:
-        if runvalues['cores'] <= 24:
-            memory = runvalues['cores'] * 4800
-        elif runvalues['cores'] == 28:
+    if runvalues["memory"] is None:
+        if runvalues["cores"] <= 24:
+            memory = runvalues["cores"] * 4800
+        elif runvalues["cores"] == 28:
             memory = 59000
     else:
         memory = 59000
@@ -270,97 +294,148 @@ def create_run_file(output, runvalues):
     # #SBATCH --ntasks=48
     # #SBATCH --ntasks-per-node=24
     # #SBATCH --threads-per-core=1
-    out = ['#!/bin/bash\n',
-           '#SBATCH -J ' + shlexnames['inputfile'] + '\n',
-           '#SBATCH --constraint=' + runvalues['cluster_section'] + '\n'
-           '#SBATCH --mail-type=ALL\n',
-           '#SBATCH --mail-user=user@server.org\n',
-           '#SBATCH --nodes=' + str(runvalues['nodes']) + '\n']
-    if runvalues['nproc_in_input']:
-        out.extend(['#SBATCH --ntasks=' + str(runvalues['cores']) + '\n'])
-    out.extend(['#SBATCH --mem=' + str(runvalues['memory']) + '\n',
-                '#SBATCH --time=' + runvalues['walltime'] + '\n',
-                '#SBATCH --output=' + shlexnames['basename'] + '.slurmout\n',
-                '\n'])
+    out = [
+        "#!/bin/bash\n",
+        "#SBATCH -J " + shlexnames["inputfile"] + "\n",
+        "#SBATCH --constraint=" + runvalues["cluster_section"] + "\n"
+        "#SBATCH --mail-type=ALL\n",
+        "#SBATCH --mail-user=user@server.org\n",
+        "#SBATCH --nodes=" + str(runvalues["nodes"]) + "\n",
+    ]
+    if runvalues["nproc_in_input"]:
+        out.extend(["#SBATCH --ntasks=" + str(runvalues["cores"]) + "\n"])
+    out.extend(
+        [
+            "#SBATCH --mem=" + str(runvalues["memory"]) + "\n",
+            "#SBATCH --time=" + runvalues["walltime"] + "\n",
+            "#SBATCH --output=" + shlexnames["basename"] + ".slurmout\n",
+            "\n",
+        ]
+    )
 
-    out.extend(['# Load modules necessary for ORCA\n',
-                'module purge\n',
-                'module load intel/18.0\n',
-                'module load openmpi/intel/2.0.4\n',
-                '\n'])
+    out.extend(
+        [
+            "# Load modules necessary for ORCA\n",
+            "module purge\n",
+            "module load intel/18.0\n",
+            "module load openmpi/intel/2.0.4\n",
+            "\n",
+        ]
+    )
 
     # OCCIGEN stuff
-    if runvalues['nproc_in_input']:
-        out.extend(['export OMP_NUM_THREADS=$SLURM_NTASKS\n', '\n'])
+    if runvalues["nproc_in_input"]:
+        out.extend(["export OMP_NUM_THREADS=$SLURM_NTASKS\n", "\n"])
     else:
-        out.extend(["# Compute actual cpu number\n",
-                    "NCPU=$(lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l)\n",
-                    "export OMP_NUM_THREADS=$NCPU\n",
-                    '\n'])
-    out.extend(['export KMP_AFFINITY=compact,1,0\n', '\n'])
+        out.extend(
+            [
+                "# Compute actual cpu number\n",
+                "NCPU=$(lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l)\n",
+                "export OMP_NUM_THREADS=$NCPU\n",
+                "\n",
+            ]
+        )
+    out.extend(["export KMP_AFFINITY=compact,1,0\n", "\n"])
 
     # Setup NBO if necessary
-    if runvalues['nbo']:
-        out.extend(['# Setup NBO6\n',
-                    'export NBOBIN=$SHAREDHOMEDIR/nbo6/bin\n',
-                    'export PATH=$PATH:$NBOBIN\n',
-                    '\n'])
+    if runvalues["nbo"]:
+        out.extend(
+            [
+                "# Setup NBO6\n",
+                "export NBOBIN=$SHAREDHOMEDIR/nbo6/bin\n",
+                "export PATH=$PATH:$NBOBIN\n",
+                "\n",
+            ]
+        )
 
     # Setup Orca
-    out.extend(['# Setup Scratch\n',
-                'export ORCA_TMPDIR=$SCRATCHDIR/orca/$SLURM_JOBID\n',
-                'mkdir -p $ORCA_TMPDIR\n',
-                '\n',
-                '# Setup Orca\n',
-                'export ORCA_BIN_DIR=$SHAREDHOMEDIR/orca-4_2_0-shared\n',
-                'export PATH=$PATH:$ORCA_BIN_DIR\n',
-                'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORCA_BIN_DIR\n',
-                '\n',
-                '# Copy input file\n',
-                'cp -f ' + shlexnames['inputfile'] + ' $ORCA_TMPDIR\n\n'])
+    out.extend(
+        [
+            "# Setup Scratch\n",
+            "export ORCA_TMPDIR=$SCRATCHDIR/orca/$SLURM_JOBID\n",
+            "mkdir -p $ORCA_TMPDIR\n",
+            "\n",
+            "# Setup Orca\n",
+            "export ORCA_BIN_DIR=$SHAREDHOMEDIR/orca-4_2_0-shared\n",
+            "export PATH=$PATH:$ORCA_BIN_DIR\n",
+            "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORCA_BIN_DIR\n",
+            "\n",
+            "# Copy input file\n",
+            "cp -f " + shlexnames["inputfile"] + " $ORCA_TMPDIR\n\n",
+        ]
+    )
 
     # Launch the actual process
-    out.extend(['cd $ORCA_TMPDIR\n',
-                '\n',
-                '# Print job info in output file\n',
-                'echo "job_id : $SLURM_JOB_ID"\n',
-                'echo "job_name : $SLURM_JOB_NAME"\n',
-                'echo "node_number : $SLURM_JOB_NUM_NODES nodes"\n',
-                'echo "core number : $SLURM_NTASKS cores"\n',
-                'echo "Node list : $SLURM_JOB_NODELIST"\n',
-                '\n'])
+    out.extend(
+        [
+            "cd $ORCA_TMPDIR\n",
+            "\n",
+            "# Print job info in output file\n",
+            'echo "job_id : $SLURM_JOB_ID"\n',
+            'echo "job_name : $SLURM_JOB_NAME"\n',
+            'echo "node_number : $SLURM_JOB_NUM_NODES nodes"\n',
+            'echo "core number : $SLURM_NTASKS cores"\n',
+            'echo "Node list : $SLURM_JOB_NODELIST"\n',
+            "\n",
+        ]
+    )
     # runtime is walltime minus one minute (with at least one minute)
-    walltime = [int(x) for x in runvalues['walltime'].split(':')]
+    walltime = [int(x) for x in runvalues["walltime"].split(":")]
     runtime = max(3600 * walltime[0] + 60 * walltime[1] + walltime[2] - 60, 60)
     # If nproc was not defined, put it in the header of the file
-    if not(runvalues['nproc_in_input']):
-        out.extend(['# Add nprocs directive to header of ' + shlexnames['inputfile'] + '\n',
-                    "sed -i '1s;^;%pal\\n  nprocs \"$NCPU\"\\nend\\n\\n;' " + shlexnames['inputfile'] + '\n',
-                    '\n'])
+    if not (runvalues["nproc_in_input"]):
+        out.extend(
+            [
+                "# Add nprocs directive to header of " + shlexnames["inputfile"] + "\n",
+                "sed -i '1s;^;%pal\\n  nprocs \"$NCPU\"\\nend\\n\\n;' "
+                + shlexnames["inputfile"]
+                + "\n",
+                "\n",
+            ]
+        )
     # Starting orca for good
-    out.extend(['# Start ORCA\n',
-                'timeout ' + str(runtime) + ' $ORCA_BIN_DIR/orca ' + shlexnames['inputfile'],
-                ' > ' + shlexnames['basename'] + '.out\n',
-                '\n'])
+    out.extend(
+        [
+            "# Start ORCA\n",
+            "timeout "
+            + str(runtime)
+            + " $ORCA_BIN_DIR/orca "
+            + shlexnames["inputfile"],
+            " > " + shlexnames["basename"] + ".out\n",
+            "\n",
+        ]
+    )
 
-    out.extend(['# Move files back to original directory\n',
-                'cp ' + shlexnames['basename'] + '.out $SLURM_SUBMIT_DIR\n',
-                'cp ' + shlexnames['basename'] + '.gbw $SLURM_SUBMIT_DIR\n',
-                'cp ' + shlexnames['basename'] + '.xyz $SLURM_SUBMIT_DIR\n',
-                '\n'])
-    if runvalues['nbo']:
-        out.extend(['# Retrieve NBO Files\n',
-                    'cp ' + runvalues['nbo_basefilename'] + '.*'
-                    ' $SLURM_SUBMIT_DIR\n'
-                    '\n'])
-    out.extend(['# Empty scratch directory\n',
-                'rm -rf $ORCA_TMPDIR\n',
-                '\n',
-                'echo "Computation finished."\n',
-                '\n'])
+    out.extend(
+        [
+            "# Move files back to original directory\n",
+            "cp " + shlexnames["basename"] + ".out $SLURM_SUBMIT_DIR\n",
+            "cp " + shlexnames["basename"] + ".gbw $SLURM_SUBMIT_DIR\n",
+            "cp " + shlexnames["basename"] + ".xyz $SLURM_SUBMIT_DIR\n",
+            "\n",
+        ]
+    )
+    if runvalues["nbo"]:
+        out.extend(
+            [
+                "# Retrieve NBO Files\n",
+                "cp " + runvalues["nbo_basefilename"] + ".*"
+                " $SLURM_SUBMIT_DIR\n"
+                "\n",
+            ]
+        )
+    out.extend(
+        [
+            "# Empty scratch directory\n",
+            "rm -rf $ORCA_TMPDIR\n",
+            "\n",
+            'echo "Computation finished."\n',
+            "\n",
+        ]
+    )
 
     # Write .sh file
-    with open(output, 'w') as script_file:
+    with open(output, "w") as script_file:
         script_file.writelines(out)
 
 
@@ -388,5 +463,5 @@ core.
 """
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
