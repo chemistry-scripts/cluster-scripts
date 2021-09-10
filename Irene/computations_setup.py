45acdba2d8f50c1b06d09941dba8ba66da9a64c9
Emmanuel <LaTruelle@users.noreply.github.com>
2021-09-10 17:39:20 +0200

    Initial Irene commit

---
+++ Irene/computations_setup.py
@@ -0,0 +1,454 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Helper functions to set up theoretical chemistry computations on computing clusters.
+
+Last Update 2020-03-17 by Emmanuel Nicolas
+email emmanuel.nicolas -at- cea.fr
+Requires Python3 to be installed.
+"""
+
+import os
+import shlex
+import logging
+import re
+
+
+class Computation:
+    """
+    Class representing the computation that will be ran.
+    """
+
+    def __init__(self, input_file, software, cmdline_args):
+        """
+        Class Builder
+        """
+        self.__input_file = input_file
+        self.__software = software
+        self.__runvalues = self.default_run_values()
+        self.fill_from_commandline(cmdline_args)
+        self.fill_missing_values()
+        self.shlexnames = self.create_shlexnames()
+
+    @property
+    def runvalues(self):
+        """Dict containing all data required to run the computation."""
+        return self.__runvalues
+
+    @runvalues.setter
+    def runvalues(self, value):
+        self.__runvalues = value
+
+    @property
+    def input_file(self):
+        """Input File name."""
+        return self.__input_file
+
+    @input_file.setter
+    def input_file(self, value):
+        self.__input_file = value
+
+    @property
+    def software(self):
+        """Software name."""
+        return self.__software
+
+    @software.setter
+    def software(self, value):
+        self.__software = value
+
+    @property
+    def walltime(self):
+        """Walltime."""
+        return self.__runvalues["walltime"]
+
+    @staticmethod
+    def default_run_values():
+        """Fill default runvalues."""
+        # Setup runvalues
+        runvalues = dict.fromkeys(
+            [
+                "inputfile",
+                "outputfile",
+                "nodes",
+                "cores",
+                "walltime",
+                "memory",
+                "gaussian_memory",
+                "chk",
+                "oldchk",
+                "rwf",
+                "nproc_in_input",
+                "memory_in_input",
+                "nbo",
+                "nbo_basefilename",
+                "cluster_section",
+            ]
+        )
+        runvalues["inputfile"] = ""
+        runvalues["outputfile"] = ""
+        runvalues["nodes"] = 1
+        runvalues["cores"] = ""
+        runvalues["walltime"] = "20:00:00"
+        runvalues["memory"] = 4000  # In MB
+        runvalues["gaussian_memory"] = 1000  # in MB
+        runvalues["chk"] = set()
+        runvalues["oldchk"] = set()
+        runvalues["rwf"] = set()
+        runvalues["nproc_in_input"] = False
+        runvalues["memory_in_input"] = False
+        runvalues["nbo"] = False
+        runvalues["nbo_basefilename"] = ""
+        runvalues["cluster_section"] = "HSW24"
+        return runvalues
+
+    def get_values_from_gaussian_input_file(self):
+        """Get core/memory values from input file, reading the Mem and NProcShared parameters."""
+        with open(self.input_file, "r") as file:
+            # Go through lines and test if they are containing nproc, mem, etc. related
+            # directives.
+            for line in file.readlines():
+                if "%nproc" in line.lower():
+                    self.runvalues["nproc_in_input"] = True
+                    self.runvalues["cores"] = int(line.split("=")[1].rstrip("\n"))
+                if "%chk" in line.lower():
+                    self.runvalues["chk"].add(line.split("=")[1].rstrip("\n"))
+                if "%oldchk" in line.lower():
+                    self.runvalues["oldchk"].add(line.split("=")[1].rstrip("\n"))
+                if "%rwf" in line.lower():
+                    self.runvalues["rwf"].add(line.split("=")[1].rstrip("\n"))
+                if "%mem" in line.lower():
+                    self.runvalues["memory_in_input"] = True
+                    mem_line = line.split("=")[1].rstrip("\n")
+                    mem_value, mem_unit = re.match(
+                        r"(\d+)([a-zA-Z]+)", mem_line
+                    ).groups()
+                    if mem_unit.upper() == "GB":
+                        self.runvalues["gaussian_memory"] = int(mem_value) * 1024
+                    elif mem_unit.upper() == "GW":
+                        self.runvalues["gaussian_memory"] = int(mem_value) * 1024 / 8
+                    elif mem_unit.upper() == "MB":
+                        self.runvalues["gaussian_memory"] = int(mem_value)
+                    elif mem_unit.upper() == "MW":
+                        self.runvalues["gaussian_memory"] = int(mem_value) / 8
+                if "nbo6" in line.lower() or "npa6" in line.lower():
+                    self.runvalues["nbo"] = True
+                if "FILE=" in line:
+                    # TITLE=FILENAME
+                    self.runvalues["nbo_basefilename"] = line.split("=")[1].rstrip(
+                        " \n"
+                    )
+
+    def fill_missing_values(self):
+        """Compute and fill all missing values."""
+        # Setup cluster_section according to number of cores
+        if not self.runvalues["nproc_in_input"]:
+            self.runvalues["cluster_section"] = "HSW24|BDW28"
+        elif self.runvalues["cores"] <= 24:
+            self.runvalues["cluster_section"] = "HSW24"
+        elif self.runvalues["cores"] <= 28:
+            self.runvalues["cluster_section"] = "BDW28"
+        elif self.runvalues["cores"] > 28 and self.runvalues["nodes"] == 1:
+            raise ValueError("Number of cores cannot exceed 28 for one node.")
+        elif self.runvalues["nodes"] > 1:
+            raise ValueError(
+                "Multiple nodes not supported on this cluster for gaussian."
+            )
+
+        memory, gaussian_memory = self.compute_memory()
+        self.runvalues["memory"] = memory
+        self.runvalues["gaussian_memory"] = gaussian_memory
+
+        if memory - gaussian_memory < 6000:
+            # Too little overhead
+            raise ValueError("Too much memory required for Gaussian to run properly")
+        if gaussian_memory > 160000:
+            # Too much memory
+            raise ValueError("Exceeded max allowed memory")
+
+    def create_shlexnames(self):
+        """Return dictionary containing shell escaped names for all possible files."""
+        shlexnames = dict()
+        input_basename = os.path.splitext(self.runvalues["inputfile"])[0]
+        shlexnames["inputfile"] = shlex.quote(self.runvalues["inputfile"])
+        shlexnames["basename"] = shlex.quote(input_basename)
+        if self.runvalues["chk"] is not None:
+            shlexnames["chk"] = [shlex.quote(chk) for chk in self.runvalues["chk"]]
+        if self.runvalues["oldchk"] is not None:
+            shlexnames["oldchk"] = [
+                shlex.quote(oldchk) for oldchk in self.runvalues["oldchk"]
+            ]
+        if self.runvalues["rwf"] is not None:
+            shlexnames["rwf"] = [shlex.quote(rwf) for rwf in self.runvalues["rwf"]]
+        return shlexnames
+
+    def fill_from_commandline(self, cmdline_args):
+        """Merge command line arguments into runvalues."""
+        self.runvalues["inputfile"] = cmdline_args["inputfile"]
+        if cmdline_args["nodes"]:
+            self.runvalues["nodes"] = cmdline_args["nodes"]
+        if cmdline_args["cores"]:
+            self.runvalues["cores"] = cmdline_args["cores"]
+        if cmdline_args["walltime"]:
+            self.runvalues["walltime"] = cmdline_args["walltime"]
+        if cmdline_args["memory"]:
+            self.runvalues["memory"] = cmdline_args["memory"]
+
+    def compute_memory(self):
+        """
+        Return ideal memory value for Jean Zay.
+
+        160GB available per core : remove 6 Gb for overhead.
+        """
+        memory = 160000
+        gaussian_memory = 0
+
+        if self.runvalues["memory_in_input"]:
+            # Memory defined in input file
+            gaussian_memory = self.runvalues["gaussian_memory"]
+        else:
+            gaussian_memory = 140000
+
+        return memory, gaussian_memory
+
+    def walltime_as_list(self):
+        """Return walltime as list: [20,00,00]"""
+        return [int(x) for x in self.runvalues["walltime"].split(":")]
+
+    def create_run_file(self, output):
+        """
+        Create .sh file that contains the script to actually run on the server.
+
+        Structure:
+            - SBATCH instructions for the queue manager
+            - setup of Gaussian16 on the nodes
+            - creation of scratch, copy necessary files
+            - Run Gaussian16
+            - Copy appropriate files back to $HOME
+            - Cleanup scratch
+
+        Instructions adapted from www.cines.fr
+        """
+        # Setup logging
+        logger = logging.getLogger()
+
+        # Setup names to use in file
+        logger.debug("Runvalues:  %s", self.runvalues)
+        logger.debug("Shlexnames: %s", self.shlexnames)
+
+        out = [
+            "#!/bin/bash\n",
+            "#SBATCH -J " + self.shlexnames["inputfile"] + "\n",
+            "#SBATCH --mail-type=ALL\n",
+            "#SBATCH --account=yck@cpu\n",
+            "#SBATCH --mail-user=user@server.org\n",
+            "#SBATCH --partition=cpu_p1\n",
+            "#SBATCH --nodes=1\n",
+            "#SBATCH --hint=nomultithread\n",
+        ]
+        # Change qos if longer walltimes
+        walltime = [int(x) for x in self.runvalues["walltime"].split(":")]
+        if walltime[0] * 3600 + walltime[1] * 60 + walltime[2] > 72000:  # > 20h
+            out.extend(["#SBATCH --qos=qos_cpu-t4\n"])
+        else:
+            out.extend(["#SBATCH --qos=qos_cpu-t3\n"])
+        if self.runvalues["nproc_in_input"]:
+            out.extend(["#SBATCH --ntasks=" + str(self.runvalues["cores"]) + "\n"])
+        else:
+            out.extend(["#SBATCH --ntasks=40\n"])
+        out.extend(
+            [
+                "#SBATCH --mem=" + str(self.runvalues["memory"]) + "\n",
+                "#SBATCH --time=" + self.runvalues["walltime"] + "\n",
+                "#SBATCH --output=" + self.shlexnames["basename"] + ".slurmout\n",
+                "\n",
+            ]
+        )
+        if not self.runvalues["nproc_in_input"]:  # nproc line not in input
+            out.extend(
+                [
+                    "# Compute actual cpu number\n",
+                    "NCPU=$(lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l)\n\n",
+                ]
+            )
+        if self.__software == "g09":
+            out.extend(
+                [
+                    "# Load Gaussian Module\n",
+                    "module purge\n",
+                    "module load gaussian/g09-revD01\n",
+                    "\n",
+                    "# Setup Gaussian specific variables\n",
+                    "export g09root='/gpfslocalsup/prod/g09/rev-C01/'\n",
+                    "source $g09root/g09/bsd/g09.profile\n",
+                ]
+            )
+        elif self.__software == "g16":
+            out.extend(
+                [
+                    "# Load Gaussian Module\n",
+                    "module purge\n",
+                    "module load gaussian/g16-revC01\n",
+                    "\n",
+                    "# Setup Gaussian specific variables\n",
+                    "export g16root='/gpfslocalsup/prod/g16/rev-C01/'\n",
+                    "source $g16root/g16/bsd/g16.profile\n",
+                ]
+            )
+        if self.runvalues["nproc_in_input"]:
+            out.extend(["export OMP_NUM_THREADS=$SLURM_JOB_CPUS_PER_NODE\n", "\n"])
+        else:
+            out.extend(["export OMP_NUM_THREADS=$NCPU\n", "\n"])
+        if self.runvalues["nbo"]:
+            out.extend(
+                [
+                    "# Setup NBO6\n",
+                    "export NBOBIN=$SHAREDHOMEDIR/nbo6/bin\n",
+                    "export PATH=$PATH:$NBOBIN\n",
+                    "\n",
+                ]
+            )
+        out.extend(
+            [
+                "# Setup Scratch\n",
+                "export GAUSS_SCRDIR=$SCRATCH/gaussian/$SLURM_JOB_ID\n",
+                "mkdir -p $GAUSS_SCRDIR\n",
+                "\n",
+                "# Copy input file\n",
+                "cp -f " + self.shlexnames["inputfile"] + " $GAUSS_SCRDIR\n\n",
+            ]
+        )
+        # If chk file is defined in input and exists, copy it in scratch
+        if self.runvalues["chk"] != set():
+            out.extend("# Copy chk file in scratch if it exists\n")
+            for chk in self.shlexnames["chk"]:
+                out.extend(
+                    [
+                        "if [ -f " + chk + " ] \n",
+                        "then\n",
+                        "  cp " + chk + " $GAUSS_SCRDIR\n",
+                        "fi\n\n",
+                    ]
+                )
+        # If oldchk file is defined in input and exists, copy it in scratch
+        if self.runvalues["oldchk"] != set():
+            out.extend("# Copy oldchk file in scratch if it exists\n")
+            for oldchk in self.shlexnames["oldchk"]:
+                out.extend(
+                    [
+                        "if [ -f " + oldchk + " ] \n",
+                        "then\n",
+                        "  cp " + oldchk + " $GAUSS_SCRDIR\n",
+                        "fi\n\n",
+                    ]
+                )
+        # If rwf file is defined in input and exists, copy it in scratch
+        if self.runvalues["rwf"] != set():
+            out.extend("# Copy rwf file in scratch if it exists\n")
+            for rwf in self.shlexnames["rwf"]:
+                out.extend(
+                    [
+                        "if [ -f " + rwf + " ] \n",
+                        "then\n",
+                        "  cp " + rwf + " $GAUSS_SCRDIR\n",
+                        "fi\n\n",
+                    ]
+                )
+        out.extend(
+            [
+                "cd $GAUSS_SCRDIR\n",
+                "\n",
+                "# Print job info in output file\n",
+                'echo "job_id : $SLURM_JOB_ID"\n',
+                'echo "job_name : $SLURM_JOB_NAME"\n',
+                'echo "node_number : $SLURM_JOB_NUM_NODES nodes"\n',
+                'echo "core number : $SLURM_JOB_CPUS_PER_NODE cores"\n',
+                'echo "Node list : $SLURM_JOB_NODELIST"\n',
+                "\n",
+            ]
+        )
+
+        # Build and add Gaussian starting line
+        out.extend(["# Start Gaussian\n"])
+        gaussian_start_line = self.gaussian_start_line()
+        out.extend(gaussian_start_line)
+
+        out.extend(
+            [
+                "# Move files back to original directory\n",
+                "cp " + self.shlexnames["basename"] + ".log $SLURM_SUBMIT_DIR\n",
+                "\n",
+            ]
+        )
+        out.extend(
+            [
+                "# If chk file exists, create fchk and copy everything\n",
+                "for f in $GAUSS_SCRDIR/*.chk; do\n",
+                '    [ -f "$f" ] && formchk $f\n',
+                "done\n",
+                "\n",
+                "for f in $GAUSS_SCRDIR/*chk; do\n",
+                '    [ -f "$f" ] && cp $f $SLURM_SUBMIT_DIR\n',
+                "done\n",
+                "\n",
+            ]
+        )
+        if self.runvalues["nbo"]:
+            out.extend(
+                [
+                    "# Retrieve NBO Files\n",
+                    "cp " + self.runvalues["nbo_basefilename"] + ".*"
+                    " $SLURM_SUBMIT_DIR\n"
+                    "\n",
+                ]
+            )
+        out.extend(
+            [
+                "# If Gaussian crashed or was stopped somehow, copy the rwf\n",
+                "for f in $GAUSS_SCRDIR/*rwf; do\n",
+                "    mkdir -p $SCRATCH/gaussian/rwf\n"
+                # Move rwf as JobName_123456.rwf to the rwf folder in scratch
+                '    [ -f "$f" ] && mv $f $SCRATCH/gaussian/rwf/'
+                + self.shlexnames["basename"]
+                + "_$SLURM_JOB_ID.rwf\n",
+                "done\n",
+                "\n",
+                "# Empty Scratch directory\n",
+                "rm -rf $GAUSS_SCRDIR\n",
+                "\n",
+                'echo "Computation finished."\n',
+                "\n",
+            ]
+        )
+
+        # Write .sh file
+        with open(output, "w") as script_file:
+            script_file.writelines(out)
+
+    def gaussian_start_line(self):
+        """Start line builder"""
+
+        # Create timeout line
+        walltime = self.walltime_as_list()
+        runtime = 3600 * walltime[0] + 60 * walltime[1] + walltime[2] - 60
+        start_line = "timeout " + str(runtime) + " "
+
+        # g09 or g16
+        start_line += self.__software + " "
+
+        # Manage processors
+        if not self.runvalues["nproc_in_input"]:
+            # nproc line not in input, set proc number as command-line argument
+            start_line += '-c="0-$(($NCPU-1))" '
+        if not self.runvalues["memory_in_input"]:
+            # memory line not in input, set it as command-line argument
+            start_line += "-m=" + str(self.runvalues["gaussian_memory"]) + "MB "
+
+        # Add input file
+        start_line += "< " + self.shlexnames["inputfile"]
+
+        # Add output file
+        start_line += " >" + self.shlexnames["basename"] + ".log\n"
+
+        return start_line
---
+++ Irene/subg16.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Entry point for submission of Gaussian16 calculations on Jean Zay
+
+Last Update 2020-03-17 by Emmanuel Nicolas
+email emmanuel.nicolas -at- cea.fr
+Requires Python3 to be installed.
+"""
+
+import argparse
+import logging
+import os
+import shlex
+import sys
+
+from computations_setup import Computation
+
+
+def main():
+    """
+    Set up the computation and submit it to the job scheduler.
+
+    Precedence of parameters for submission:
+        - Command line parameters
+        - Gaussian script
+        - Default parameters
+
+    Structure of program:
+    - Define runvalues:
+        - Fill Defaults
+        - Parse file and replace appropriately
+        - Get command-line parameters and replace appropriately
+    - Compute missing values
+        - Memory
+        - Number of nodes if appropriate
+        - Cluster section (HSW24/BDW28)
+        - Convert filenames to printable values
+    - Check parameters
+        - Existence of files (or non-existence...)
+        - Compatibility nproc/nodes/memory
+    - Build script file
+    - Submit script
+    """
+    # Setup logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(message)s")
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    # Get parameters from command line
+    cmdline_args = get_options()
+
+    # Retrieve input file name, create output file name
+    input_file_name = cmdline_args["inputfile"]
+    script_file_name = input_file_name + ".sh"
+
+    # Check existence of input file
+    if not os.path.exists(input_file_name):
+        print("=========== !!! WARNING !!! ===========")
+        print("The input file was not found.")
+        sys.exit()
+    # Check that file input.sh does not exist, not to start twice the same job
+    if os.path.exists(script_file_name):
+        print("=========== !!! WARNING !!! ===========")
+        print(" The corresponding .sh file already exists ")
+        print("Make sure it is not a mistake, erase it and rerun the script")
+        print("Alternatively, you can submit the job directly with:")
+        print("sbatch {0}".format(shlex.quote(script_file_name)))
+        sys.exit()
+    # Avoid end of line problems due to conversion between Windows and Unix
+    # file endings
+    #    os.system("dos2unix {0}".format(shlex.quote(input_file_name)))
+    text = open(input_file_name, "r").read().replace("\r\n", "\n")
+    open(input_file_name, "w").write(text)
+
+    # Create computation object
+    computation = Computation(input_file_name, "g16", cmdline_args)
+
+    # Create run file for gaussian
+    computation.create_run_file(script_file_name)
+
+    # Submit the script
+    os.system("sbatch {0}".format(shlex.quote(script_file_name)))
+    print(
+        "job {0} submitted with a walltime of {1} hours".format(
+            input_file_name, computation.walltime
+        )
+    )
+
+
+def get_options():
+    """Check command line options and accordingly set computation parameters."""
+    parser = argparse.ArgumentParser(
+        description=help_description(), epilog=help_epilog()
+    )
+    parser.formatter_class = argparse.RawDescriptionHelpFormatter
+    parser.add_argument(
+        "-p", "--proc", type=int, help="Number of processors used for the computation",
+    )
+    parser.add_argument(
+        "-n", "--nodes", type=int, help="Number of nodes used for the computation"
+    )
+    parser.add_argument(
+        "-t",
+        "--walltime",
+        default="20:00:00",
+        type=str,
+        help="Maximum time allowed for the computation",
+    )
+    parser.add_argument(
+        "-m",
+        "--memory",
+        type=int,
+        help="Amount of memory allowed for the computation, in MB",
+    )
+    parser.add_argument("inputfile", type=str, nargs=1, help="The input file to submit")
+
+    try:
+        args = parser.parse_args()
+    except argparse.ArgumentError as error:
+        print(str(error))  # Print something like "option -a not recognized"
+        sys.exit(2)
+
+    # Get values from parser
+    cmdline_args = dict.fromkeys(["inputfile", "walltime", "memory", "cores", "nodes"])
+    cmdline_args["inputfile"] = os.path.basename(args.inputfile[0])
+    cmdline_args["walltime"] = args.walltime
+    if args.proc:
+        cmdline_args["cores"] = args.proc
+    if args.nodes:
+        cmdline_args["nodes"] = args.nodes
+    if args.memory:
+        cmdline_args["memory"] = args.memory
+
+    return cmdline_args
+
+
+def help_description():
+    """Return description of program for help message."""
+    return """
+Setup and submit a job to the SLURM queueing system on the OCCIGEN cluster.
+The job script name should end with .gjf or .com.
+"""
+
+
+def help_epilog():
+    """Return additional help message."""
+    return """
+Defaults values:
+  Default memory:          54GB
+  Default cores:           24
+  Default walltime:        24:00:00
+
+Values for number of cores used and memory to use are read in the input file,
+but are overridden if command line arguments are provided.
+
+When using shared nodes (less than 24 cores), the default memory is 4GB per
+core.
+"""
+
+
+if __name__ == "__main__":
+    main()
